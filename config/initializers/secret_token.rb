70157f398e053d5081b94c7001f4923191aa8dac
Todd Warren <todd@warrenfamily.org>
2012-01-07 18:14:14 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendpics::Application.config.secret_token = '65673edc40715606b41101ce4d066ab9cd9ec35678bcc2d9bbb6b360feb517530bbe969fe4bcdba68ca2eb36e38d0156e2a3b43cf8ae6ddc9d5e5b37cc38e01b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Friendpics::Application.config.session_store :cookie_store, :key => '_friendpics_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Friendpics::Application.config.session_store :active_record_store
